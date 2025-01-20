6118505ddc16bf9dcc30a0fa6ddc728e4a0c27c9
DanielDuda260802 <dudadaniel2608@gmail.com>
2025-01-20 20:52:09 +0100

    Builded a Django Docker Container

--- chesshub_project/chesshub_project/settings.py
+++ chesshub_project/chesshub_project/settings.py
@@ -22,13 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-secret-key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
