963a7dc285dfd26fd88b91ee74c7a58b2cc6d6f7
DanielDuda260802 <dudadaniel2608@gmail.com>
2025-01-24 00:00:19 +0100

    feat: Register Celery Beat tasks in Django admin
    - Added Celery Beat periodic task management to Django admin.
    - Enabled monitoring and manual triggering of scheduled tasks.

--- chesshub_project/main/admin.py
+++ chesshub_project/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PGNFile, Game
+from .models import PGNFile, Game, FENPosition
 
 # Register your models here.
 admin.site.register(Game)
+admin.site.register(FENPosition)
 admin.site.register(PGNFile)
